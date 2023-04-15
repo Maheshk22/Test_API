0f8a402a398337b790e0858dafb9771c3b9823e8
Maheshk22 <mkcs11341@gmail.com>
2023-04-16 01:13:09 +0530

    Add project files.

---
+++ Test_API/Data/Model/Leads_API.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_API.Data.Model
+{
+    public class Leads_API
+    {
+        public class CustomData
+        {
+            public string type { get; set; }
+            public int unitCount { get; set; }
+            public bool instantRental { get; set; }
+        }
+
+        public class Datum
+        {
+            public string uuid { get; set; }
+            public string type { get; set; }
+            public string status { get; set; }
+            public string expiresAt { get; set; }
+            public bool instantRental { get; set; }
+            public string createdAt { get; set; }
+            public string updatedAt { get; set; }
+            public Needs needs { get; set; }
+            public List<Record> records { get; set; }
+            public Store store { get; set; }
+            public List<State> states { get; set; }
+            public object conversation { get; set; }
+        }
+
+        public class Link
+        {
+            public string url { get; set; }
+            public string label { get; set; }
+            public bool active { get; set; }
+            public string first { get; set; }
+            public string last { get; set; }
+            public object prev { get; set; }
+            public string next { get; set; }
+        }
+
+        public class Meta
+        {
+            public int current_page { get; set; }
+            public int from { get; set; }
+            public int last_page { get; set; }
+            public List<Link> links { get; set; }
+            public string path { get; set; } = null;
+            public int per_page { get; set; }
+            public int to { get; set; }
+            public int total { get; set; }
+        }
+
+        public class Needs
+        {
+            public string duration { get; set; }
+            public string moveInDate { get; set; }
+            public string typesOfItems { get; set; }
+            public string reasonForStorage { get; set; }
+        }
+
+        public class Record
+        {
+            public string type { get; set; }
+            public int quoteId { get; set; }
+            public Reservation reservation { get; set; }
+        }
+
+        public class Reservation
+        {
+            public string TAX { get; set; }
+            public int? DOOR { get; set; }
+            public string ICON { get; set; }
+            public string DEPTH { get; set; }
+            public string PRICE { get; set; }
+            public string WIDTH { get; set; }
+            public int STATUS { get; set; }
+            public int ACCT_ID { get; set; }
+            public int CHANNEL { get; set; }
+            public int CLIMATE { get; set; }
+            public DateTime CREATED { get; set; }
+            public string DEPOSIT { get; set; }
+            public int SITE_ID { get; set; }
+            public int SQ_FEET { get; set; }
+            public string TOT_AMT { get; set; }
+            public int UNIT_ID { get; set; }
+            public object DISC_AMT { get; set; }
+            public string DOOR_VAL { get; set; }
+            public int FEATURES { get; set; }
+            public object MKT_CODE { get; set; }
+            public int QUOTE_ID { get; set; }
+            public bool RENT_NOW { get; set; }
+            public string ACCT_NAME { get; set; }
+            public string ADMIN_FEE { get; set; }
+            public string PUSH_RATE { get; set; }
+            public int? RENTAL_ID { get; set; }
+            public string CLASS_TYPE { get; set; }
+            public int CREATED_BY { get; set; }
+            public DateTime EXPIRATION { get; set; }
+            public object MKT_SOURCE { get; set; }
+            public int OBJ_PERIOD { get; set; }
+            public int QUOTE_TYPE { get; set; }
+            public int UPDATED_BY { get; set; }
+            public int? ACCESS_TYPE { get; set; }
+            public int ATTRIBUTE01 { get; set; }
+            public int ATTRIBUTE02 { get; set; }
+            public string CLIMATE_VAL { get; set; }
+            public int? GATE_KEYPAD { get; set; }
+            public string PRORATE_AMT { get; set; }
+            public string RES_DEPOSIT { get; set; }
+            public string STREET_RATE { get; set; }
+            public string UNIT_NUMBER { get; set; }
+            public string FEATURES_VAL { get; set; }
+            public object RES_GROUP_ID { get; set; }
+            public int SITE_CLASS_ID { get; set; }
+            public int INQUIRY_SOURCE { get; set; }
+            public int OBJ_PERIOD_UOM { get; set; }
+            public TRANQUOTEPCD TRAN_QUOTE_PCD { get; set; }
+            public string ACCESS_TYPE_VAL { get; set; }
+            public string ATTRIBUTE01_VAL { get; set; }
+            public string ATTRIBUTE02_VAL { get; set; }
+            public string CREATED_BY_NAME { get; set; }
+            public string START_RENT_RATE { get; set; }
+            public DateTime QUOTE_START_DATE { get; set; }
+            public int LOST_DEMAND_REASON { get; set; }
+            public string OBJ_PERIOD_UOM_VAL { get; set; }
+            public TRANQUOTEPCDDETAIL TRAN_QUOTE_PCD_DETAIL { get; set; }
+            public TRANQUOTENOTESDETAIL TRAN_QUOTE_NOTES_DETAIL { get; set; }
+        }
+
+        public class Root
+        {
+            public List<Datum> data { get; set; }
+            public Link links { get; set; }
+            public Meta meta { get; set; }
+        }
+
+        public class State
+        {
+            public string state { get; set; }
+            public CustomData customData { get; set; }
+            public DateTime createdAt { get; set; }
+            public User user { get; set; }
+        }
+
+        public class Store
+        {
+            public string name { get; set; }
+            public string identifier { get; set; }
+        }
+
+        public class TRANQUOTENOTESDETAIL
+        {
+        }
+
+        public class TRANQUOTEPCD
+        {
+        }
+
+        public class TRANQUOTEPCDDETAIL
+        {
+        }
+
+        public class User
+        {
+            public int id { get; set; }
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public string name { get; set; }
+            public string jobTitle { get; set; }
+            public string employmentDate { get; set; }
+            public string email { get; set; }
+            public bool demoAccount { get; set; }
+            public string profilePictureUrl { get; set; }
+            public object deletedAt { get; set; }
+        }
+    }
+}
---
+++ Test_API/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+using static Test_API.Program;
+using static System.Net.Mime.MediaTypeNames;
+using System.Json;
+using Test_API.Data;
+using Microsoft.TeamFoundation;
+using System.Runtime.Remoting.Lifetime;
+using static Test_API.Data.Model.Leads_API;
+
+namespace Test_API
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Root obj = new Root();
+
+                //Your Code Here for view
+                Console.WriteLine("Schedular run at:"+DateTime.Now);
+
+                // call api from server
+                string satrtdate = "2022-09-01 00:00:00";
+                string enddate = "2022-09-10 00:00:00";
+
+                Root getapidata = GetDataFromAPI(satrtdate, enddate);
+                bool issavestatus = saveAPIData(getapidata);
+                if (issavestatus)
+                    Console.WriteLine("data save successfully");
+                else
+                    Console.WriteLine("Something went wrong");
+
+                int milliseconds = 300000;  //300000 milliseconds = 5 minutes
+                Thread.Sleep(milliseconds);
+            }
+        }
+
+        private static Root GetDataFromAPI(string startdate, string enddate)
+        {
+            var parameters = new Dictionary<string, string>() {
+                            { "start", startdate },
+                            { "end", enddate }
+                            };
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //client.DefaultRequestHeaders.Add("APIKEYPARAM", "9|i7n9vPfpWjpiH5KUP95gBVNhXuzWdyc2IuPphYKK");
+                client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", "9|i7n9vPfpWjpiH5KUP95gBVNhXuzWdyc2IuPphYKK");
+                var baseUrl = "https://api.csrscape.com/api/yardi/leads";
+                var url = baseUrl + "?" + string.Join("&", parameters.Select(x => string.Format("{0}={1}", x.Key, x.Value)));
+                var task = client.GetAsync(url);
+                task.Wait();
+
+                if (!task.Result.IsSuccessStatusCode)
+                {
+                    throw new Exception(task.Result.StatusCode.ToString());
+                }
+
+                var readTask = task.Result.Content.ReadAsStringAsync();
+                var settings = new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore,
+                    MissingMemberHandling = MissingMemberHandling.Ignore
+                };
+                Root jsonModel = JsonConvert.DeserializeObject<Root>(readTask.Result, settings);
+                readTask.Wait();
+                return jsonModel;
+            }
+        }
+
+        private static bool saveAPIData(Root getapidata)
+        {
+            try
+            {
+                Console_ApiEntities db = new Console_ApiEntities();
+                foreach (var item in getapidata.data)
+                {
+                    // insert record in leads table
+                    Lead lead = new Lead();
+                    lead.Uuid = Guid.Parse(item.uuid);
+                    lead.Type = item.type;
+                    lead.Status = item.status;
+                    lead.ExpiresAt = item.expiresAt;
+                    lead.InstantRental = Convert.ToBoolean(item.instantRental);
+                    lead.CreatedAt = item.createdAt;
+                    lead.UpdatedAt = item.updatedAt;
+                    db.Leads.Add(lead);
+                    db.SaveChanges();
+                    // insert record in needs table
+                    Need needs = new Need();
+                    needs.NeedId = Guid.NewGuid();
+                    needs.Duration = item.needs.duration;
+                    needs.MoveInDate = item.needs.moveInDate;
+                    needs.TypesOfItems = item.needs.typesOfItems;
+                    needs.ReasonForStorage = item.needs.reasonForStorage;
+                    db.Needs.Add(needs);
+                    db.SaveChanges();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+      
+
+
+
+    }
+}
